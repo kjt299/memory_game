2315871e836efaebf6c235304a3467798bb5b353
kjt299 <47479824+kjt299@users.noreply.github.com>
2019-10-02 11:58:40 +0100

    clean and refactor code

--- behaviour.js
+++ behaviour.js
@@ -1,35 +1,26 @@
-const color = {
-  blue: "images/blue.jpg",
-  red: "images/red.jpg",
-  yellow: "images/yellow.jpg",
-  green: "images/green.jpg",
-  pink: "images/pink.jpg",
-  orange: "images/orange.jpg",
-  navy: "images/navy.jpg",
-  black: "images/black.jpg",
-  back: "images/back.jpg",
-  transparent: "images/transparent.jpg"
-};
-
 const game = {
   locations: ["a1","a2","a3","a4","b1","b2","b3","b4","c1","c2","c3","c4","d1","d2","d3","d4"],
-  colors: [
-    color.blue,
-    color.red,
-    color.yellow,
-    color.green,
-    color.pink,
-    color.orange,
-    color.navy,
-    color.black
-  ],
+  colors: {
+    blue: "images/blue.jpg",
+    red: "images/red.jpg",
+    yellow: "images/yellow.jpg",
+    green: "images/green.jpg",
+    pink: "images/pink.jpg",
+    orange: "images/orange.jpg",
+    navy: "images/navy.jpg",
+    black: "images/black.jpg",
+  },
+  default: {
+    back: "images/back.jpg",
+    transparent: "images/transparent.jpg"
+  },
   generatedRandomLocations: [],
   generatedRandomColors: [],
   content: [],
-  started: false,
   guessedCards: [],
-  clickedCard: null,
   counter: 0,
+  started: false,
+  clickedCard: null,
   cardOne: null,
   cardTwo: null
 };
@@ -55,13 +46,12 @@ function reset() {
 
 function createGame() {
   let x = 0;
-  while (x < game.colors.length) {
+  while (x < Object.keys(game.colors).length) {
     const chosenRandomColor = chooseRandomColor();
     let y = 0;
     while (y < 2) {
-      let chosenRandomLocation = chooseRandomLocation();
-      let aCard = new Card(chosenRandomColor, chosenRandomLocation);
-      game.content.push(aCard);
+      const chosenRandomLocation = chooseRandomLocation();
+      game.content.push(new Card(chosenRandomColor, chosenRandomLocation));
       y++;
     }
     x++;
@@ -70,24 +60,22 @@ function createGame() {
 }
 
 function chooseRandomColor() {
-  let randomColor = Math.floor(Math.random() * game.colors.length);
+  const colorsArray = Object.keys(game.colors);
+  const randomColor = Math.floor(Math.random() * colorsArray.length);
   if (game.generatedRandomColors.includes(randomColor) === false) {
     game.generatedRandomColors.push(randomColor);
-    console.log("Random Color created");
-    return game.colors[randomColor];
+    return game.colors[colorsArray[randomColor]];
   } else {
     return chooseRandomColor();
   }
 }
 
 function chooseRandomLocation() {
-  let randomLocation = Math.floor(Math.random() * game.locations.length);
+  const randomLocation = Math.floor(Math.random() * game.locations.length);
   if (game.generatedRandomLocations.includes(randomLocation) === false) {
     game.generatedRandomLocations.push(randomLocation);
-    console.log("Random Location created");
     return game.locations[randomLocation];
   } else {
-    console.log("callback");
     return chooseRandomLocation();
   }
 }
@@ -95,7 +83,7 @@ function chooseRandomLocation() {
 //sets all cards to have the same color (back) when first loaded
 function printCards() {
   for (let i = 0; i < game.locations.length; i++) {
-    $("#" + game.locations[i]).attr("src", color.back);
+    $("#" + game.locations[i]).attr("src", game.default.back);
   }
 }
 
@@ -114,11 +102,11 @@ function compareCards() {
 }
 
 function revertCard(card) {
-  $("#" + card).attr("src", color.back);
+  $("#" + card).attr("src", game.default.back);
 }
 
 function removeCard(card) {
-  $("#" + card).attr("src", color.transparent);
+  $("#" + card).attr("src", game.default.transparent);
 }
 
 function revealCard() {
@@ -152,14 +140,14 @@ $("img").click(function(event) {
 });
 
 function gameOver() {
-  if (game.guessedCards.length / 2 === game.colors.length) {
+  if (game.guessedCards.length / 2 === Object.keys(game.colors).length) {
     if (innerWidth < 768 && innerWidth > 500) {
       // show penguin picture
       $(".col-lg-5 img").show();
     }
     $("#table").hide();
-    $("#start button").attr("disabled", true);
     $("#start button")
+      .attr("disabled", true)
       .html(
         "<b>Congratulations!</b><br />Number of moves: <b>" +
           game.counter +
@@ -172,11 +160,12 @@ function gameOver() {
       $("#start button")
         .html("Start!")
         .removeClass("congrats")
-        .addClass("btn");
-      $("#start button").attr("disabled", false);
+        .addClass("btn")
+        .attr("disabled", false);
     }, 5000);
   }
 }
+
 
 $(document).ready(function() {
   $("#start").click(function() {
